59e134a0614715c08261bb8cac1601132bf06573
Donghwi_Park <gptnc5100@gmail.com>
2024-10-07 09:47:04 +0200

    Binding between controls

--- Maui_DatabindingDemo/App.xaml.cs
+++ Maui_DatabindingDemo/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Maui_DatabindingDemo
+﻿using Maui_DatabindingDemo.Pages;
+
+namespace Maui_DatabindingDemo
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new SliderPage();
         }
     }
 }
---
+++ Maui_DatabindingDemo/Pages/SliderPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Maui_DatabindingDemo.Pages;
+
+public partial class SliderPage : ContentPage
+{
+	public SliderPage()
+	{
+		InitializeComponent();
+	}
+}
